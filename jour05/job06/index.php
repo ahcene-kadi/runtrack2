0934650a41ce0bcab938a0c590e70b30ac980bd1
ahcene-kadi <71874119+ahcene-kadi@users.noreply.github.com>
2020-11-07 23:12:43 +0100

    remplace echo -> return

--- jour05/job06/index.php
+++ jour05/job06/index.php
@@ -7,10 +7,10 @@ $str="Une supernova est l'ensemble des phénomènes qui résultent de l'implosio
     function leetspeak($str)
     {
  
-         echo  str_replace(["a","A","b","B","e","é","è","E","g","G","l","L","s","S","t","T"], ["4","4","8","8","3","3","3","3","6","6","1","1","5","5","7","7"], $str); 
+         return  str_replace(["a","A","b","B","e","é","è","E","g","G","l","L","s","S","t","T"], ["4","4","8","8","3","3","3","3","6","6","1","1","5","5","7","7"], $str); 
          
      }
  
- leetspeak($str);
+ echo leetspeak($str);
 
  ?>
